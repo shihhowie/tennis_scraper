1018e0351b09ad8aec6b7442fb2f4bfd8612857a
EC2 Default User <ec2-user@ip-172-31-24-168.eu-north-1.compute.internal>
2025-03-17 14:12:26 +0000

    tennis scraper app react

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import axios from 'axios';
+
+const App = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get("16.16.66.189:5000/api/availability").then(response => {
+      const formattedEvents = response.data.map(item => ({
+        title: item.slots>0 ? "Available" : "Fully Booked",
+        start: `${item.date}T${String(item.start).padStart(2, '0')}:00:00`,
+        backgroundColor: item.slots>0 ? "green" : "red"
+      }));
+      setEvents(formattedEvents);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Tennis Court Availability</h1>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" events={events} />
+    </div>
+  );
+};
+
+export default App;
+
