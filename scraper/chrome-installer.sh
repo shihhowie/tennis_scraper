4b7a584a8ebf548d95bc09ec0a36878a99505c58
howardshih <howardshihhoward@gmail.com>
2025-03-21 22:34:56 +0000

    update docker file

--- scraper/chrome-installer.sh
+++ scraper/chrome-installer.sh
@@ -1,7 +1,7 @@
-wget https://storage.googleapis.com/chrome-for-testing-public/134.0.6998.165/linux64/chrome-linux64.zip
+curl -o chrome-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/134.0.6998.165/linux64/chrome-linux64.zip
 unzip chrome-linux64.zip
 chmod +x chrome-linux64/chrome
 
-wget https://storage.googleapis.com/chrome-for-testing-public/134.0.6998.165/linux64/chromedriver-linux64.zip
+curl -o chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/134.0.6998.165/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
 chmod +x chromedriver-linux64/chromedriver
